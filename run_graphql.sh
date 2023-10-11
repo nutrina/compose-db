6392d208df1b55f8e34f3196eb33d3a450070a64
Gerald Iakobinyi-Pich <nutrina9@gmail.com>
2023-10-11 12:42:30 +0300

    Testing VC wrapper

--- run_graphql.sh
+++ run_graphql.sh
@@ -1,2 +1,3 @@
 
-composedb graphql:server --ceramic-url=http://127.0.0.1:7007 --graphiql gitcoin-passport-vc-runtime.json --did-private-key=${PRIVAKE_KEY} --port=5005
+# composedb graphql:server --ceramic-url=http://127.0.0.1:7007 --graphiql gitcoin-passport-vc-runtime.json --did-private-key=${PRIVAKE_KEY} --port=5005
+composedb graphql:server --ceramic-url=http://127.0.0.1:7007 --graphiql gitcoin-passport-vc-wrapper-runtime.json --did-private-key=${PRIVAKE_KEY} --port=5005
