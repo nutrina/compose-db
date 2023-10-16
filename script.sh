b710ef57d5373e4ee392ed8b501d14ac5edd9e8e
Gerald Iakobinyi-Pich <nutrina9@gmail.com>
2023-10-16 15:54:39 +0300

    Testing with staging node ...

--- script.sh
+++ script.sh
@@ -1,5 +1,11 @@
 
 
+# CERAMIC_URL=http://localhost:7007 
+CERAMIC_URL=https://ceramic-gitcoin-staging.hirenodes.io
+
+echo
+echo "Creating did"
+composedb did:from-private-key ${PRIVAKE_KEY}
 
 echo
 echo "Creating composite"
@@ -7,7 +13,7 @@ composedb composite:create gitcoin-passport-vc.graphql --output=gitcoin-passport
 
 echo
 echo "Deploy composite"
-composedb composite:deploy gitcoin-passport-vc-composite.json --ceramic-url=http://localhost:7007 --did-private-key=${PRIVAKE_KEY} | jq .
+composedb composite:deploy gitcoin-passport-vc-composite.json --ceramic-url=${CERAMIC_URL} --did-private-key=${PRIVAKE_KEY} | jq .
 
 echo
 echo "List models"
@@ -16,9 +22,9 @@ composedb composite:models gitcoin-passport-vc-composite.json | jq .
 
 echo
 echo "Compile the composite"
-composedb composite:compile gitcoin-passport-vc-composite.json src/__generated__/definition.js --ceramic-url=http://localhost:7007
+composedb composite:compile gitcoin-passport-vc-composite.json src/__generated__/definition.js --ceramic-url=${CERAMIC_URL}
 
 
 echo
 echo "Compile the composite to json"
-composedb composite:compile gitcoin-passport-vc-composite.json gitcoin-passport-vc-runtime.json --ceramic-url=http://localhost:7007
+composedb composite:compile gitcoin-passport-vc-composite.json gitcoin-passport-vc-runtime.json --ceramic-url=${CERAMIC_URL}
